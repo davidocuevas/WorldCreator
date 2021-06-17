acd45f27213f7c2cc5fd6e900dd939f30e892765
davidocuevas <davidocuevas01@gmail.com>
2021-06-17 12:02:03 -0700

    Create AnimalType.java

---
+++ AnimalType.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * AnimalType class: Public enum for Animals
+ * @author Rainier Getuaban, David Cuevas, Ernesto Diaz, Josh Reyes
+ *
+ */
+public enum AnimalType {
+	BIRD,
+	BEAR,
+	INSECT,
+	COW
+}
