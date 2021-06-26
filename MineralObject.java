6833b24ac0a4fe2c06e32a735a9be6d4949d7a63
davidocuevas <davidocuevas01@gmail.com>
2021-06-26 14:25:41 -0700

    Create MineralObject.java

---
+++ MineralObject.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * MineralObject class: Serves as an interface for all mineral products
+ * @author Rainier Getuaban, David Cuevas, Ernesto Diaz, Josh Reyes
+ *
+ */
+public abstract class MineralObject extends TerraObject{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 *  Returns class description
+	 *  @Override toString()
+	 *  @return String
+	 */
+	public String toString() {
+		return "CheckInput Class is designed to serve as an interface for all mineral products";
+	}
+}
