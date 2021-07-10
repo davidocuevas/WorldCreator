bd05119f9082705c2ac73ecf3e09ebeff4addc34
davidocuevas <davidocuevas01@gmail.com>
2021-07-10 16:47:27 -0700

    Create MineralType.java

---
+++ MineralType.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * MineralType class: Public enum for Minerals
+ * @author Rainier Getuaban, David Cuevas, Ernesto Diaz, Josh Reyes
+ *
+ */
+public enum MineralType {
+	COAL,
+	LEAD,
+	SALT,
+	TOPSOIL
+}
