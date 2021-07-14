2d1296b48a480a29e92cd16ba4629f0040236c8f
davidocuevas <davidocuevas01@gmail.com>
2021-07-13 19:05:39 -0700

    Create PlantType.java

---
+++ PlantType.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * PlantType class: Public enum for Plants
+ * @author Rainier Getuaban, David Cuevas, Ernesto Diaz, Josh Reyes
+ *
+ */
+public enum PlantType {
+	APPLETREE,
+	GRASS,
+	FLOWER,
+	VINES
+}
