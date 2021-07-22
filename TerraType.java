9c9d7f058cc11795d5553d05060833fade3e02c3
davidocuevas <davidocuevas01@gmail.com>
2021-07-21 18:03:28 -0700

    Create TerraType.java

---
+++ TerraType.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * TerraType class: Public enum for Terras
+ * @author Rainier Getuaban, David Cuevas, Ernesto Diaz, Josh Reyes
+ *
+ */
+public enum TerraType {
+	PLANT,
+	ANIMAL,
+	WATER,
+	MINERAL
+}
