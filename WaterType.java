4267f4ae31c547f8a07d7b173ef3018b7e8fbb3f
davidocuevas <davidocuevas01@gmail.com>
2021-07-28 19:34:05 -0700

    Create WaterType.java

---
+++ WaterType.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * WaterType class: Public enum for Waters
+ * @author Rainier Getuaban, David Cuevas, Ernesto Diaz, Josh Reyes
+ *
+ */
+public enum WaterType {
+	POND,
+	OCEAN,
+	LAKE,
+	RIVER
+}
